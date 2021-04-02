01311d526f94726db987f87bcd3ba398d6bf4b90
Özgem Elif Acar <72570228+ozgemacar@users.noreply.github.com>
2021-04-02 23:17:26 +0300

    Update follower_tweepy.py

--- follower_tweepy.py
+++ follower_tweepy.py
@@ -14,10 +14,10 @@ import pandas as pd
 from google.colab import drive
 drive.mount('/content/gdrive')
 
-!ls "/content/gdrive/MyDrive/Colab Notebooks/twitterapp-ozgem.txt"  #reads the text file from Drive OR add your own directory. 
+!ls "/content/gdrive/MyDrive/..."                               #Include the trajectory that constains the text file of your Twitter API's access tokens and keys. 
 
 config=configparser.RawConfigParser()
-config.read(filenames= r'/content/gdrive/MyDrive/Colab Notebooks/twitterapp-ozgem.txt')
+config.read(filenames= r'/content/gdrive/MyDrive/...')          #Include the trajectory that constains the text file of your Twitter API's access tokens and keys. 
 print(config.sections())
 
 access_token=config.get("twitterapp_ozgem", "access_token")
@@ -29,11 +29,11 @@ auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret) 
 api = tw.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-#the following code obtains follower's user objects and save as a json file.
+# The following code obtains follower's user objects and save as a json file.
 
-screen_name ="SCREEN_NAME" #Insert the screen name that you want to get followers from.
+screen_name ="SCREEN_NAME" #Insert the Twitter screen name of the user from who you want to get followers.
 
-json_file = open("DATE-SCREEN_NAME-ozgem.json", "a")
+json_file = open("DATE-SCREEN_NAME-YOURNAME.json", "a")      # This is the format I use for saving user object json files from specific users as date that I retrieved the data, the user's screen name and my name.
 for follower in tw.Cursor(api.followers, screen_name).items():
   json_str = json.dumps(follower._json)
   json_file.write(json_str + "\n")
